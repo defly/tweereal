ad61c215f1be6d79cc0b262085d84f675a11381d
defly <defly@clean.(none)>
2012-04-30 19:55:47 +0300

    rewrite filter

--- api-client/restreamer.js
+++ api-client/restreamer.js
@@ -1,85 +1,98 @@
 var authConf = require('./apiConf.js').authConf;
-// commit2
+
 var reqConf = {
-	port:443,
-	host:'stream.twitter.com',
-	https:true,
-	path:'/1/statuses/filter.json',
-	oauth_signature:"",
-	method:'POST',
-	body:{
-		locations:'-180,-90,180,90'
+	port: 443,
+	host: 'stream.twitter.com',
+	https: true,
+	path: '/1/statuses/filter.json',
+	oauth_signature: "",
+	method: 'POST',
+	body: {
+		locations: '-180,-90,180,90'
 	}
 };
 
-var streamer = new (require("./lib/Streamer.js").Streamer)(authConf, reqConf);
-var restreamer = new (require("./lib/RestreamServer.js").RestreamServer)(8090, "127.0.0.1");
+var streamer = new(require("./lib/Streamer.js").Streamer)(authConf, reqConf);
+var restreamer = new(require("./lib/RestreamServer.js").RestreamServer)(8090, "127.0.0.1");
 
-var roundWithDepth = function(x,depthNum) {
-	return Math.round(x*depthNum) / depthNum;
-};
+var roundWithDepth = function(x, depthNum) {
+		return Math.round(x * depthNum) / depthNum;
+	};
 
-var boundingToCoordinates = function(bounding, /*maximal long*/max) {
-	var deltaLat = bounding[2][1] - bounding[0][1];
-	var deltaLng = bounding[2][0] - bounding[0][0];
+var boundingToCoordinates = function(bounding, /*maximal long*/ max) {
+		var deltaLat = bounding[2][1] - bounding[0][1];
+		var deltaLng = bounding[2][0] - bounding[0][0];
 
-	if ((deltaLat > max) || (deltaLng > max)) return false;
+		if ((deltaLat > max) || (deltaLng > max)) return false;
 
-	var lat = bounding[0][1] + deltaLat * Math.random();
-	var lng = bounding[0][0] + deltaLng * Math.random();
-	var precision = Math.max(deltaLat, deltaLng);
+		var lat = bounding[0][1] + deltaLat * Math.random();
+		var lng = bounding[0][0] + deltaLng * Math.random();
+		var precision = Math.max(deltaLat, deltaLng);
 
-	var depthNum = 100000;
+		var depthNum = 100000;
 
-	return	[
-		roundWithDepth(lat,depthNum), roundWithDepth(lng, depthNum), roundWithDepth(precision, depthNum) //accuracy
-	];
-};
+		return [
+		roundWithDepth(lat, depthNum), roundWithDepth(lng, depthNum), roundWithDepth(precision, depthNum) //accuracy
+		];
+	};
 
-var protocolCodes = {
-	"exact": 0, //tweete with exact coordinates
-	"bounding": 1, //with bounding box locations
-	"limit": 2 //
-};
+var Quality = function() {
+		var lastTrack = 0;
+		var tweets = 0;
+		this.getQuality = function(track) {
+			var delta = track - lastTrack;
+			if (delta < 0) {
+				delta = track;
+			}
+			var quality = tweets / (tweets + delta);
+			lastTrack = track;
+			tweets = 0;
+			return roundWithDepth(quality, 1000);
+		};
+		this.upTweets = function() {
+			tweets++;
+		};
+	};
 
-// var analyze = require('Sentimental').analyze;
+var q = new Quality();
 
-var filter = function(tweet) {
-	var coordinates;
-	var code;
-
-	if (tweet.limit !== undefined) {
-		return false;
-	}
-	// console.log(analyze(tweet.text).score,tweet.text); //sentiment
-	if (tweet.coordinates) {
-		if ((tweet.coordinates.coordinates[0] === 0 && tweet.coordinates.coordinates[1] === 0)) {
-			return false;
-		}
-		coordinates = tweet.coordinates.coordinates.reverse();
-		code = protocolCodes["exact"];
-	} else if (tweet.place) {
-		var boundCoordinates = boundingToCoordinates(tweet.place.bounding_box.coordinates[0], 2);
-		if (boundCoordinates !== false) {
-			coordinates = boundCoordinates;
-			code = protocolCodes["bounding"];
+var customFilter = function(tweet) {
+		if (tweet.coordinates) {
+			filters.exact(tweet);
+			q.upTweets();
+		} else if (tweet.place) {
+			filters.notExact(tweet);
+			q.upTweets();
+		} else if (typeof tweet.limit !== "undefined") {
+			filters.limit(tweet);
 		} else {
+			console.log("else", tweet);
 			return false;
 		}
-	} else if (tweet.limit) {
-		console.log(tweet);
-		code = protocolCodes["limit"];
-		restreamer.write(code + " " + tweet.limit.track + "\r\n");
-		return false;
-	} else {
-		console.log("else",tweet);
-		return false;
-	}
+	};
 
-	restreamer.write(code + " " + coordinates.join(" ") + "\r\n");
-}
+var filters = {};
+
+filters.exact = function(tweet) {
+	var coordinates = tweet.coordinates.coordinates.reverse();
+	if (!(coordinates[0] === 0 && coordinates[1] === 0)) {
+		restreamer.write("0 " + coordinates[0] + " " + coordinates[1] + "\r\n");
+	}
+};
+
+filters.notExact = function(tweet) {
+	var boundCoordinates = boundingToCoordinates(tweet.place.bounding_box.coordinates[0], 2);
+	if (boundCoordinates !== false) {
+		restreamer.write("1 " + boundCoordinates[0] + " " + boundCoordinates[1] + " " + boundCoordinates[2] + "\r\n");
+	}
+};
+
+filters.limit = function(tweet) {
+	var quality = q.getQuality(tweet.limit.track);
+	restreamer.write("2 " + quality + "\r\n");
+};
 
 streamer.stream();
 streamer.on("tweet", function(tweet) {
-	filter(tweet);
+	customFilter(tweet);
 });
