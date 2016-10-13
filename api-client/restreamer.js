aa9415ac34a9a0779e5a5a7147e2d1bf0a729871
Artem Yavorsky <aqson@me.com>
2016-10-13 04:33:25 +0300

    Normalize indentation.

--- api-client/restreamer.js
+++ api-client/restreamer.js
@@ -1,101 +1,101 @@
 var authConf = require('./apiConf.js').authConf;
 var reqConf = {
-	port: 443,
-	host: 'stream.twitter.com',
-	https: true,
-	path: '/1/statuses/filter.json',
-	oauth_signature: "",
-	method: 'POST',
-	body: {
-		locations: '-180,-90,180,90'
-	}
+  port: 443,
+  host: 'stream.twitter.com',
+  https: true,
+  path: '/1/statuses/filter.json',
+  oauth_signature: "",
+  method: 'POST',
+  body: {
+    locations: '-180,-90,180,90'
+  }
 };
 var redis = require("redis");
 var streamer = new(require("./lib/Streamer.js").Streamer)(authConf, reqConf);
 var filters = {};
 
 var roundWithDepth = function(x, depthNum) {
-		return Math.round(x * depthNum) / depthNum;
-	};
+    return Math.round(x * depthNum) / depthNum;
+  };
 
 var boundingToCoordinates = function(bounding, /*maximal long*/ max) {
-		var deltaLat = bounding[2][1] - bounding[0][1];
-		var deltaLng = bounding[2][0] - bounding[0][0];
+    var deltaLat = bounding[2][1] - bounding[0][1];
+    var deltaLng = bounding[2][0] - bounding[0][0];
 
-		if ((deltaLat > max) || (deltaLng > max)) return false;
+    if ((deltaLat > max) || (deltaLng > max)) return false;
 
-		var lat = bounding[0][1] + deltaLat * Math.random();
-		var lng = bounding[0][0] + deltaLng * Math.random();
-		var precision = Math.max(deltaLat, deltaLng);
+    var lat = bounding[0][1] + deltaLat * Math.random();
+    var lng = bounding[0][0] + deltaLng * Math.random();
+    var precision = Math.max(deltaLat, deltaLng);
 
-		var depthNum = 100000;
+    var depthNum = 100000;
 
-		return [
-		roundWithDepth(lat, depthNum), roundWithDepth(lng, depthNum), roundWithDepth(precision, depthNum) //accuracy
-		];
-	};
+    return [
+    roundWithDepth(lat, depthNum), roundWithDepth(lng, depthNum), roundWithDepth(precision, depthNum) //accuracy
+    ];
+  };
 
 var Quality = function() {
-		var lastTrack = 0;
-		var tweets = 0;
-		this.getQuality = function(track) {
-			var delta = track - lastTrack;
-			if (delta < 0) {
-				delta = track;
-			}
-			var quality = tweets / (tweets + delta);
-			lastTrack = track;
-			tweets = 0;
-			return roundWithDepth(quality, 1000);
-		};
-		this.upTweets = function() {
-			tweets++;
-		};
-	};
+    var lastTrack = 0;
+    var tweets = 0;
+    this.getQuality = function(track) {
+      var delta = track - lastTrack;
+      if (delta < 0) {
+        delta = track;
+      }
+      var quality = tweets / (tweets + delta);
+      lastTrack = track;
+      tweets = 0;
+      return roundWithDepth(quality, 1000);
+    };
+    this.upTweets = function() {
+      tweets++;
+    };
+  };
 
 var q = new Quality();
 
 var customFilter = function(tweet) {
-		if (tweet.coordinates) {
-			filters.exact(tweet);
-			q.upTweets();
-		} else if (tweet.place) {
-			filters.notExact(tweet);
-			q.upTweets();
-		} else if (typeof tweet.limit !== "undefined") {
-			filters.limit(tweet);
-		} else {
-			console.log("else", tweet);
-			return false;
-		}
-	};
+    if (tweet.coordinates) {
+      filters.exact(tweet);
+      q.upTweets();
+    } else if (tweet.place) {
+      filters.notExact(tweet);
+      q.upTweets();
+    } else if (typeof tweet.limit !== "undefined") {
+      filters.limit(tweet);
+    } else {
+      console.log("else", tweet);
+      return false;
+    }
+  };
 
 var redisClient = redis.createClient();
 
 var write = function(message) {
-		redisClient.publish("T", message);
-	};
+    redisClient.publish("T", message);
+  };
 
 filters.exact = function(tweet) {
-	var coordinates = tweet.coordinates.coordinates.reverse();
-	if (!(coordinates[0] === 0 && coordinates[1] === 0)) {
-		write("0 " + coordinates[0] + " " + coordinates[1] + "\r\n");
-	}
+  var coordinates = tweet.coordinates.coordinates.reverse();
+  if (!(coordinates[0] === 0 && coordinates[1] === 0)) {
+    write("0 " + coordinates[0] + " " + coordinates[1] + "\r\n");
+  }
 };
 
 filters.notExact = function(tweet) {
-	var boundCoordinates = boundingToCoordinates(tweet.place.bounding_box.coordinates[0], 2);
-	if (boundCoordinates !== false) {
-		write("1 " + boundCoordinates[0] + " " + boundCoordinates[1] + " " + boundCoordinates[2] + "\r\n");
-	}
+  var boundCoordinates = boundingToCoordinates(tweet.place.bounding_box.coordinates[0], 2);
+  if (boundCoordinates !== false) {
+    write("1 " + boundCoordinates[0] + " " + boundCoordinates[1] + " " + boundCoordinates[2] + "\r\n");
+  }
 };
 
 filters.limit = function(tweet) {
-	var quality = q.getQuality(tweet.limit.track);
-	write("2 " + quality + "\r\n");
+  var quality = q.getQuality(tweet.limit.track);
+  write("2 " + quality + "\r\n");
 };
 
 streamer.stream();
 streamer.on("tweet", function(tweet) {
-	customFilter(tweet);
+  customFilter(tweet);
 });
--- web-server/static/interface.js
+++ web-server/static/interface.js
@@ -1,132 +1,132 @@
 Activity.Interface = function() {
-	var options = Activity.Bubble.prototype.options;
+  var options = Activity.Bubble.prototype.options;
 
-	var sliders = [{
-		"id": "all-sliders-size",
-		"value": options.baseSize * 2,
-		"handler": function(x) {
-			options.baseSize = x;
-			options.size = Math.round(Math.sqrt(options.zoom) * x);
-		},
-		"transform": function(y) {
-			// return Math.round(Math.sqrt(options.zoom) * y * 0.5);
-			return y * 0.5;
-		}
-	}, {
-		"id": "all-sliders-live",
-		"value": options.live / 100,
-		"handler": function(x) {
-			options.live = x;
-		},
-		"transform": function(y) {
-			return Math.round(y * 100);
-		}
-	}, {
-		"id": "exact-opacity",
-		"value": 100,
-		"handler": function(x) {
-			options.neatFillOpacity = options.baseFillOpacity * x;
-			options.neatStrokeOpacity = options.baseStrokeOpacity * x;
-		},
-		"transform": function(y) {
-			return 0.01 * Math.round(y);
-		}
-	}, {
-		"id": "places-opacity",
-		"value": 50,
-		"handler": function(x) {
-			options.blurFillOpacity = options.baseFillOpacity * x;
-			options.blurStrokeOpacity = options.baseStrokeOpacity * x;
-		},
-		"transform": function(y) {
-			return 0.01 * Math.round(y);
-		}
-	}, {
-		"id": "places-precision",
-		"value": 50,
-		"handler": function(x) {
-			options.precision = x;
-		},
-		"transform": function(y) {
-			return 0.02*Math.round(y);
-		}
-	}];
+  var sliders = [{
+    "id": "all-sliders-size",
+    "value": options.baseSize * 2,
+    "handler": function(x) {
+      options.baseSize = x;
+      options.size = Math.round(Math.sqrt(options.zoom) * x);
+    },
+    "transform": function(y) {
+      // return Math.round(Math.sqrt(options.zoom) * y * 0.5);
+      return y * 0.5;
+    }
+  }, {
+    "id": "all-sliders-live",
+    "value": options.live / 100,
+    "handler": function(x) {
+      options.live = x;
+    },
+    "transform": function(y) {
+      return Math.round(y * 100);
+    }
+  }, {
+    "id": "exact-opacity",
+    "value": 100,
+    "handler": function(x) {
+      options.neatFillOpacity = options.baseFillOpacity * x;
+      options.neatStrokeOpacity = options.baseStrokeOpacity * x;
+    },
+    "transform": function(y) {
+      return 0.01 * Math.round(y);
+    }
+  }, {
+    "id": "places-opacity",
+    "value": 50,
+    "handler": function(x) {
+      options.blurFillOpacity = options.baseFillOpacity * x;
+      options.blurStrokeOpacity = options.baseStrokeOpacity * x;
+    },
+    "transform": function(y) {
+      return 0.01 * Math.round(y);
+    }
+  }, {
+    "id": "places-precision",
+    "value": 50,
+    "handler": function(x) {
+      options.precision = x;
+    },
+    "transform": function(y) {
+      return 0.02*Math.round(y);
+    }
+  }];
 
-	for (var i = sliders.length - 1; i >= 0; i--) {
-		sliders[i]["slider"] = new Activity.Interface.Slider(sliders[i]["id"], sliders[i]["value"], sliders[i]["handler"], sliders[i]["transform"]);
-	};
+  for (var i = sliders.length - 1; i >= 0; i--) {
+    sliders[i]["slider"] = new Activity.Interface.Slider(sliders[i]["id"], sliders[i]["value"], sliders[i]["handler"], sliders[i]["transform"]);
+  };
 
-	var switcherExact = new Activity.Interface.Switcher("exact-switcher", function() {
-		Activity.Bubble.prototype.renderNeat = Activity.Bubble.prototype.renderNeatOn;
-	}, function() {
-		Activity.Bubble.prototype.renderNeat = function() {};
-	});
+  var switcherExact = new Activity.Interface.Switcher("exact-switcher", function() {
+    Activity.Bubble.prototype.renderNeat = Activity.Bubble.prototype.renderNeatOn;
+  }, function() {
+    Activity.Bubble.prototype.renderNeat = function() {};
+  });
 
-	var switcherPlaces = new Activity.Interface.Switcher("places-switcher", function() {
-		Activity.Bubble.prototype.renderBlur = Activity.Bubble.prototype.renderBlurOn;
-	}, function() {
-		Activity.Bubble.prototype.renderBlur = function() {};
-	});
+  var switcherPlaces = new Activity.Interface.Switcher("places-switcher", function() {
+    Activity.Bubble.prototype.renderBlur = Activity.Bubble.prototype.renderBlurOn;
+  }, function() {
+    Activity.Bubble.prototype.renderBlur = function() {};
+  });
 
 
-	$("#switch-defaults").on("click", function() {
-		for (var i = sliders.length - 1; i >= 0; i--) {
-			sliders[i]["slider"].toDefault();
-		}
-		switcherExact.on();
-		switcherPlaces.on();
-	});
+  $("#switch-defaults").on("click", function() {
+    for (var i = sliders.length - 1; i >= 0; i--) {
+      sliders[i]["slider"].toDefault();
+    }
+    switcherExact.on();
+    switcherPlaces.on();
+  });
 
 };
 
 Activity.Interface.Slider = function(id, value, handler, transform) {
-	var self = this;
-	this.defaults = value;
-	this.handler = handler;
-	this.transform = transform;
-	this.slider = $("#" + id).slider({
-		step: 0.01,
-		animate: true,
-		value: value,
-		slide: function(event, ui) {
-			self.handler(self.transform(ui.value));
-		}
-	});
+  var self = this;
+  this.defaults = value;
+  this.handler = handler;
+  this.transform = transform;
+  this.slider = $("#" + id).slider({
+    step: 0.01,
+    animate: true,
+    value: value,
+    slide: function(event, ui) {
+      self.handler(self.transform(ui.value));
+    }
+  });
 };
 
 Activity.Interface.Slider.prototype.toDefault = function() {
-	this.slider.slider("value", this.defaults);
-	this.handler(this.transform(this.defaults));
-	console.log(this.transform(this.defaults));
+  this.slider.slider("value", this.defaults);
+  this.handler(this.transform(this.defaults));
+  console.log(this.transform(this.defaults));
 };
 
 Activity.Interface.Switcher = function(id, switchOn, switchOff) {
-	var self = this;
-	self.$el = $("#" + id);
-	self.state = true;
-	self.switchOn = switchOn;
-	self.switchOff = switchOff;
+  var self = this;
+  self.$el = $("#" + id);
+  self.state = true;
+  self.switchOn = switchOn;
+  self.switchOff = switchOff;
 
-	self.$el.on("click", function(event) {
-		event.preventDefault();
-		if (self.state) {
-			self.off();
-		} else {
-			self.on();
-		}
-		return false;
-	});
+  self.$el.on("click", function(event) {
+    event.preventDefault();
+    if (self.state) {
+      self.off();
+    } else {
+      self.on();
+    }
+    return false;
+  });
 
 };
 
 Activity.Interface.Switcher.prototype.on = function() {
-	this.switchOn();
-	this.$el.removeClass("off").addClass("on").text("on");
-	this.state = true;
+  this.switchOn();
+  this.$el.removeClass("off").addClass("on").text("on");
+  this.state = true;
 }
 
 Activity.Interface.Switcher.prototype.off = function() {
-	this.switchOff();
-	this.$el.removeClass("on").addClass("off").text("off");
-	this.state = false;
+  this.switchOff();
+  this.$el.removeClass("on").addClass("off").text("off");
+  this.state = false;
 }
